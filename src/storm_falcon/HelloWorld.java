b5ba66a96eb869204365dc730c59e89601ed25cf
Armyadviser <874062225@qq.com>
2016-11-30 15:47:31 +0800

    add function findFiles() use Files.walkFileTree()

--- src/storm_falcon/HelloWorld.java
+++ src/storm_falcon/HelloWorld.java
@@ -1,5 +1,11 @@
 package storm_falcon;
 
+import storm_falcon.util.file.FileHelper;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by Storm_Falcon on 2015/10/10.
  *
@@ -8,7 +14,20 @@ package storm_falcon;
  */
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-		int a = (int) 1.6;
-		System.out.println(a);
+    	long n1, n2;
+
+		n1 = System.currentTimeMillis();
+		List<File> l2 = FileHelper.findFiles("e:/", "class").collect(Collectors.toList());
+		System.out.println("new:" + l2.size());
+		n2 = System.currentTimeMillis();
+		System.out.println("new:" + (n2 - n1));
+
+		n1 = System.currentTimeMillis();
+		List<String> l1 = FileHelper.traversalDir("e:/", false, "class");
+		System.out.println("old:" + l1.size());
+		n2 = System.currentTimeMillis();
+		System.out.println("old:" + (n2 - n1));
+
 	}
 }
+
