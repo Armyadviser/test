0ce40a528313bb4bc9bfc93bebddf71b5f7724ef
Armyadviser <874062225@qq.com>
2016-11-30 16:34:35 +0800

    add function findFiles() use Files.walkFileTree()

--- src/storm_falcon/HelloWorld.java
+++ src/storm_falcon/HelloWorld.java
@@ -1,11 +1,5 @@
 package storm_falcon;
 
-import storm_falcon.util.file.FileHelper;
-
-import java.io.File;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * Created by Storm_Falcon on 2015/10/10.
  *
@@ -14,19 +8,6 @@ import java.util.stream.Collectors;
  */
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-    	long n1, n2;
-
-		n1 = System.currentTimeMillis();
-		List<File> l2 = FileHelper.findFiles("e:/", "class").collect(Collectors.toList());
-		System.out.println("new:" + l2.size());
-		n2 = System.currentTimeMillis();
-		System.out.println("new:" + (n2 - n1));
-
-		n1 = System.currentTimeMillis();
-		List<String> l1 = FileHelper.traversalDir("e:/", false, "class");
-		System.out.println("old:" + l1.size());
-		n2 = System.currentTimeMillis();
-		System.out.println("old:" + (n2 - n1));
 
 	}
 }
--- src/storm_falcon/util/StringDataErrorException.java
+++ src/storm_falcon/util/StringDataErrorException.java
@@ -4,7 +4,7 @@ package storm_falcon.util;
  * Created by Storm_Falcon on 2015/10/10.
  * This will happen when a string type is error.
  */
-public class StringDataErrorException extends Exception {
+public class StringDataErrorException extends RuntimeException {
     public StringDataErrorException(String s) {
         super(s);
     }
