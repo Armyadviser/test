cb5b56abcea8553d31d02931cb9d5d0656a58d1b
gewanpeng <gewanpengge-soft.com>
2017-03-27 16:30:48 +0800

    Add db advanced queue. Upgrade ExcelReader.

---
+++ Self/src/storm_falcon/util/db/queue/DBConnection.java
@@ -0,0 +1,132 @@
+package dbqueue;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import oracle.jdbc.internal.OracleTypes;
+
+public class DBConnection {
+
+	private final static String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@218.25.253.70:1521:gebssdb";
+	private String usr = "test";
+	private String pwd = "test";
+	
+	private Connection conn = null;
+	private CallableStatement inputCs = null;
+	private CallableStatement outputCs = null;
+	private CallableStatement outputCsByGiven = null;
+	
+	static {
+		try {
+			Class.forName(DRIVER);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public DBConnection() {
+		try {
+			conn = DriverManager.getConnection(url, usr, pwd);
+			inputCs = conn.prepareCall("{call inqueue(?, ?)}");
+			outputCs = conn.prepareCall("{call outqueue(?, ?)}");
+			outputCsByGiven = conn.prepareCall("{call outqueuebyip(?, ?, ?)}");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean createQueue(String tableName, String queueName) {
+		try {
+			CallableStatement cs = conn.prepareCall("{call createqueue(?, ?)}");
+			cs.setString(1, tableName);
+			cs.setString(2, queueName);
+			cs.registerOutParameter(1, OracleTypes.VARCHAR);
+			cs.registerOutParameter(2, OracleTypes.VARCHAR);
+			return cs.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean dropQueue(String tableName, String queueName) {
+		return false;
+	}
+	
+	public boolean addLast(String ip, String queueName) {
+		try {
+			inputCs.setString(1, ip);
+			inputCs.setString(2, queueName);
+			inputCs.registerOutParameter(1, OracleTypes.VARCHAR);
+			inputCs.registerOutParameter(2, OracleTypes.VARCHAR);
+			inputCs.execute();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public String removeFirst(String queueName) {
+		try {
+			outputCs.setString(1, "");
+			outputCs.setString(2, queueName);
+			outputCs.registerOutParameter(1, OracleTypes.VARCHAR);
+			outputCs.registerOutParameter(2, OracleTypes.VARCHAR);
+			outputCs.execute();
+			String res = outputCs.getString(1);
+			return res;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+	
+	public String removeItem(String queueName, String ip) {
+		try {
+			outputCsByGiven.setString(1, "");
+			outputCsByGiven.setString(2, queueName);
+			outputCsByGiven.setString(3, ip);
+			outputCsByGiven.registerOutParameter(1, OracleTypes.VARCHAR);
+			outputCsByGiven.registerOutParameter(2, OracleTypes.VARCHAR);
+			outputCsByGiven.registerOutParameter(3, OracleTypes.VARCHAR);
+			outputCsByGiven.execute();
+			String res = outputCsByGiven.getString(1);
+			return res;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+	
+	public void close() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+			if (inputCs != null) {
+				inputCs.close();
+			}
+			if (outputCs != null) {
+				outputCs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		DBConnection conn = new DBConnection();
+		conn.createQueue("queue3", "queue3");
+//		conn.addLast("127.0.0.2", "testqueue03");
+//		String ip = conn.removeFirst("testqueue03");
+//		String ip = conn.removeItem("testqueue03", "127.0.0.2");
+//		System.out.println(ip);
+		conn.close();
+	}
+}
--- Self/src/storm_falcon/util/xls/ExcelReader.java
+++ Self/src/storm_falcon/util/xls/ExcelReader.java
@@ -5,6 +5,15 @@ import jxl.Workbook;
 
 import java.io.Closeable;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by Storm_Falcon on 2016/12/5.
@@ -16,7 +25,7 @@ public class ExcelReader implements Closeable {
 
     private Sheet sheet;
 
-    private int lineNumber = 0;
+    private int rowNumber = 0;
 
     public void open(String filePath) {
         open(filePath, 0);
@@ -31,13 +40,22 @@ public class ExcelReader implements Closeable {
         }
     }
 
+    public void open(String filePath, String sheetName) {
+        try {
+            workbook = Workbook.getWorkbook(new FileInputStream(filePath));
+            sheet = workbook.getSheet(sheetName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public boolean hasNext() {
-        return ++lineNumber < sheet.getRows();
+        return ++rowNumber < sheet.getRows();
     }
 
     public String getContent(int columnIndex) {
         try {
-            return sheet.getCell(columnIndex, lineNumber - 1).getContents();
+            return sheet.getCell(columnIndex, rowNumber - 1).getContents();
         } catch (Exception e) {
             e.printStackTrace();
             return "";
@@ -52,6 +70,59 @@ public class ExcelReader implements Closeable {
         return mapper.convert(items);
     }
 
+    private static int[] convertIndexes(String[] sIndexes) {
+        return Arrays.stream(sIndexes)
+            .map(String::toUpperCase)
+            .flatMapToInt(s -> {
+                char[] chars = s.toCharArray();
+                int result = 0;
+                for (char aChar : chars) {
+                    result += (int) aChar - 65;
+                }
+                return IntStream.of(result);
+            }).toArray();
+    }
+
+    public static <T> Stream<T> stream(String filePath, int sheet,
+                                       int rowStart, int rowEnd, String[] columnIndexes, CellMapper<T> mapper) {
+        int[] nColumnIndexes = convertIndexes(columnIndexes);
+
+        ExcelReader reader = new ExcelReader();
+        reader.open(filePath, sheet);
+        for (int i = 1; reader.hasNext() && i < rowStart; i++);
+
+        Iterator<T> iterator = new Iterator<T>() {
+            @Override
+            public boolean hasNext() {
+                return reader.hasNext() && reader.rowNumber <= rowEnd + 1;
+            }
+
+            @Override
+            public T next() {
+                return reader.getObject(nColumnIndexes, mapper);
+            }
+        };
+
+        return StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+            iterator, Spliterator.ORDERED | Spliterator.NONNULL),
+            false).onClose(asUncheckedRunnable(reader));
+    }
+
+    public static <T> Stream<T> stream(String filePath, String[] columnIndexes,
+                                       int rowStart, int rowEnd, CellMapper<T> mapper) {
+        return stream(filePath, 0, rowStart, rowEnd, columnIndexes, mapper);
+    }
+
+    private static Runnable asUncheckedRunnable(Closeable c) {
+        return () -> {
+            try {
+                c.close();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        };
+    }
+
     public void close() {
         if (workbook == null) {
             return;
@@ -59,5 +130,4 @@ public class ExcelReader implements Closeable {
         workbook.close();
         workbook = null;
     }
-
 }
