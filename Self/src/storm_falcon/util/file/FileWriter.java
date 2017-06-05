4e6220009c7a892ac9ae5959cfd9972a8e85edcb
gewanpeng <gewanpeng@ge-soft.com>
2017-06-05 13:27:17 +0800

    upgrade with synchronized and Closeable.

--- Self/src/storm_falcon/util/file/FileWriter.java
+++ Self/src/storm_falcon/util/file/FileWriter.java
@@ -1,86 +1,107 @@
 package storm_falcon.util.file;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.List;
+import java.io.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class FileWriter {
+public class FileWriter implements Closeable {
 
-	private File mFile = null;
-	
 	private BufferedWriter mWriter = null;
 
 	private String lineSeparator = System.getProperty("line.separator");
 
-	private int lineNum = 0;
-	
-	public boolean open(String strFilePath) {
-		mFile = new File(strFilePath);
-		if (!mFile.exists()) {
-			mFile = FileHelper.createFile(strFilePath);
+	private AtomicInteger lineNum;
+
+	private boolean opened;
+
+	private boolean closed;
+
+	private final Object lock = new Object();
+
+	public void open(String filePath, String encode, boolean append) {
+		synchronized (lock) {
+			File file = createFile(filePath);
+
+			mWriter = newBufferedWriter(file, encode, append);
+			opened = mWriter != null;
+
+			lineNum = new AtomicInteger(0);
 		}
-		
-		try {
-			mWriter = new BufferedWriter(new OutputStreamWriter(
-							new FileOutputStream(mFile), "GBK"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	
-	public boolean open(String strFilePath, String encoding) {
-		mFile = new File(strFilePath);
-		if (!mFile.exists()) {
-			mFile = FileHelper.createFile(strFilePath);
-		}
-		
-		try {
-			mWriter = new BufferedWriter(new OutputStreamWriter(
-							new FileOutputStream(mFile), encoding));
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
 	}
 
+	public void open(String filePath, String encode) {
+		open(filePath, encode, true);
+	}
+
+	public void open(String filePath) {
+		open(filePath, "gbk", true);
+	}
+
+	private File createFile(String strFilePath) {
+		File file = new File(strFilePath);
+		if (file.exists()) {
+			return file;
+		}
+
+		File parentFile = file.getParentFile();
+
+		if (!parentFile.exists()) {
+			try {
+				parentFile.mkdirs();
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return file;
+	}
+
+	private BufferedWriter newBufferedWriter(File file, String encode, boolean append) {
+		try {
+			return new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(file, append), encode));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 	public void setSeparator(String separator) {
 		lineSeparator = separator;
 	}
 	
-	public synchronized void writeLine(String line){
-		try {
-			mWriter.write(line);
-			mWriter.write(lineSeparator);
-			lineNum++;
-		} catch (IOException e) {
-			e.printStackTrace();
+	public void writeLine(String line){
+		synchronized (lock) {
+			if (!opened) {
+				return;
+			}
+
+			try {
+				mWriter.write(line);
+				mWriter.write(lineSeparator);
+
+				lineNum.incrementAndGet();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+
 	}
 
+	@Override
 	public void close() {
-		try {
-			mWriter.flush();
-			mWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+		synchronized (lock) {
+			if (closed) {
+				return;
+			}
 
-	public <T> void writeAll(String strFilePath, String encoding, List<T> list) {
-		open(strFilePath, encoding);
-		for (T s : list) {
-			writeLine(s.toString());
+			try {
+				mWriter.flush();
+				mWriter.close();
+				closed = true;
+				opened = false;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		close();
-	}
-
-	public void writeAll(String strFilePath, List<String> list) {
-		writeAll(strFilePath, "gbk", list);
 	}
 }
